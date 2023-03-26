2bcb873d1ac12ecb8affc96d7833b644208b31b3
prgmr99 <johnyeom24@naver.com>
2023-03-26 13:38:34 +0900

    Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const { redirect } = require("next/dist/server/api-utils");
+const API_KEY = process.env.API_KEY;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
