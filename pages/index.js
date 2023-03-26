45ef12725a7bac180499604f34842c2b7ebf58a6
prgmr99 <johnyeom24@naver.com>
2023-03-26 12:16:45 +0900

    Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
+const API_KEY = "28480f953401db60e553e41284f5f407";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
